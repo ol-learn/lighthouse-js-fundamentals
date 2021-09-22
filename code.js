ebd1dfb0c8442dbca9009ecdc26e7bbeafea09a3
Olga O <ol.learning1@gmail.com>
2021-09-22 00:16:16 +0000

    Adding JSDocs to isEven function

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
+/**
+ * isEven takes as its parameter a number and returns a Boolean value representing 
+ * whether or not the number is even.
+ * @param {*} num 
+ * @returns Returns a Boolean value representing whether or not the number is even
+ */
 const isEven = function (num) {
   return num % 2 === 0;
 }
